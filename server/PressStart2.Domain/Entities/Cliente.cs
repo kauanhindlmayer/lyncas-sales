247abe2ef51410b69310bd74bfed50d4895fb795
Kauan <kauanhindlmayer07@gmail.com>
2022-11-08 15:18:09 -0300

    Adicionando os arquivos do backend

---
+++ server/PressStart2.Domain/Entities/Cliente.cs
@@ -0,0 +1,42 @@
+﻿using PressStart2.Domain.Entities.Contracts;
+
+namespace PressStart2.Domain.Entities
+{
+    public class Cliente : EntidadeBase
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Telefone { get; private set; }
+        public string Cpf { get; private set; }
+        public bool FlagAtivo { get; private set; }
+
+        // Construtor para o Entity Framework Core
+        protected Cliente() { } 
+
+        public Cliente(string nome, string email, string telefone, string cpf)
+        {
+            Nome = nome;
+            Email = email;
+            Telefone = telefone;
+            Cpf = cpf;
+            FlagAtivo = true;
+
+            this.AdicionarClienteContract();
+        }
+
+        public void Atualizar(string nome, string email, string telefone, string cpf)
+        {
+            Nome = nome;
+            Email = email;
+            Telefone = telefone;
+            Cpf = cpf;
+
+            this.AtualizarClienteContract(); 
+        }
+
+        public void Inativar()
+        {
+            FlagAtivo = false;
+        }
+    }
+}
---
+++ server/PressStart2.Domain/Entities/Venda.cs
@@ -0,0 +1,61 @@
+﻿using PressStart2.Domain.Entities.Contracts;
+
+namespace PressStart2.Domain.Entities
+{
+    public class Venda : EntidadeBase // Agregadora - agrega funcionalidades da classe VendaItem
+    {
+        private List<VendaItem> _itens = new();
+        public Guid ClienteId { get; private set; }
+        public int QuantidadeItens { get; private set; }
+        public DateTime DataVenda { get; private set; }
+        public DateTime DataFaturamento { get; private set; }
+        public decimal ValorTotal { get; private set; }
+        public virtual IEnumerable<VendaItem> Itens => _itens;
+
+        // Propriedade de Navegabilidade
+        public virtual Cliente Cliente { get; private set; }
+
+        // Construtor para o Entity Framework Core
+        protected Venda() { }
+        
+        public Venda(Guid clienteId, int quantidadeItens, DateTime dataFaturamento, decimal valorTotal)
+        {
+            ClienteId = clienteId;
+            QuantidadeItens = quantidadeItens;
+            DataVenda = DateTime.Now;
+            DataFaturamento = dataFaturamento;
+            ValorTotal = valorTotal;
+
+            this.AdicionarVendaContract();
+        }
+
+        public void Atualizar(Guid clienteId, int quantidadeItens, DateTime dataFaturamento, decimal valorTotal)
+        {
+            ClienteId = clienteId;
+            QuantidadeItens = quantidadeItens;
+            DataFaturamento = dataFaturamento;
+            ValorTotal = valorTotal;
+
+            this.AtualizarVendaContract();
+        }
+
+        public void AdicionarItem(VendaItem vendaItem)
+        {
+            AddNotifications(vendaItem);
+            _itens.Add(vendaItem);
+        }
+
+        public void AtualizarItem(VendaItem vendaItem)
+        {
+            var vi = _itens.FirstOrDefault(p => p.Id == vendaItem.Id);
+            vi?.Atualizar(vendaItem.DescricaoItem, vendaItem.ValorUnitario, vendaItem.Quantidade, vendaItem.ValorTotal);
+
+            AddNotifications(vi);
+        }
+
+        public void RemoverItem(VendaItem vendaItem)
+        {
+            _itens.Remove(vendaItem);
+        }
+    }
+}
