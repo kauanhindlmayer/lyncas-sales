1d40521251e2f6e5d131015e180ea83df8164259
Kauan <kauanhindlmayer07@gmail.com>
2022-11-16 14:40:11 -0300

    feat: add GetAllCustomers

---
+++ backend/PressStart2/PressStart2.Domain/Commands/GetAllCustomers/GetAllCustomersHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using PressStart2.Domain.DTOs;
+using PressStart2.Domain.Interfaces.Repositories;
+using prmToolkit.NotificationPattern;
+
+namespace PressStart2.Domain.Commands.GetAllCustomers
+{
+    public class GetAllCustomersHandler : Notifiable, IRequestHandler<GetAllCustomersRequest, CommandResponse>
+    {
+        private readonly IRepositoryCustomer _repositoryCustomer;
+
+        public GetAllCustomersHandler(IRepositoryCustomer repositoryCustomer)
+        {
+            _repositoryCustomer = repositoryCustomer;
+        }
+
+        public Task<CommandResponse> Handle(GetAllCustomersRequest request, CancellationToken cancellationToken)
+        {
+            var listCustomers = _repositoryCustomer.GetAll();
+
+            return Task.FromResult(new CommandResponse(listCustomers.Select(
+                customer => new GetAllCustomersResponse(
+                    customer.Id,
+                    customer.Name,
+                    customer.Email,
+                    customer.Phone,
+                    customer.Cpf,
+                    customer.IsActive)), this));
+        }
+    }
+}
+ 
---
+++ backend/PressStart2/PressStart2.Domain/Commands/GetAllCustomers/GetAllCustomersRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using PressStart2.Domain.DTOs;
+
+namespace PressStart2.Domain.Commands.GetAllCustomers
+{
+    public class GetAllCustomersRequest : IRequest<CommandResponse>
+    {
+
+    }
+}
