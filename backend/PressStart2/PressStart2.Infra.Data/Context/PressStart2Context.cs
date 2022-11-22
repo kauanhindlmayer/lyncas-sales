93d5b57568233ed4b1f4c2ca878c22aec6b0bebd
Kauan <kauanhindlmayer07@gmail.com>
2022-11-22 15:14:17 -0300

    feat: add PressStart2Context

--- backend/PressStart2/PressStart2.Infra.Data/Context/PressStart2Context.cs
+++ backend/PressStart2/PressStart2.Infra.Data/Context/PressStart2Context.cs
@@ -1,7 +1,23 @@
-﻿namespace PressStart2.Infra.Data.Context
-{
-    public class PressStart2Context
-    {
+﻿using Microsoft.EntityFrameworkCore;
+using PressStart2.Domain.Entities;
+using prmToolkit.NotificationPattern;
 
+namespace PressStart2.Infra.Data.Context
+{
+    public class PressStart2Context : DbContext
+    {
+        public PressStart2Context(DbContextOptions<PressStart2Context> options) : base(options) { }
+        public DbSet<Customer> CustomerDbSet { get; set; }
+        public DbSet<Sale> SaleDbSet { get; set; }
+        public DbSet<SaleItem> SaleItemDbSet { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Ignore<Notification>();
+
+            // modelBuilder.ApplyConfigurationsFromAssembly(typeof(PressStart2Context).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
