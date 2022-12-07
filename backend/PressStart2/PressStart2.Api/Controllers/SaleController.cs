8d414367a0e102a5aced91dd19daef7520c38959
Kauan <kauanhindlmayer07@gmail.com>
2022-12-07 09:15:33 -0300

    feat: add Sales Configurations

--- backend/PressStart2/PressStart2.Api/Controllers/SaleController.cs
+++ backend/PressStart2/PressStart2.Api/Controllers/SaleController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PressStart2.Domain.Commands.CreateSale;
 using PressStart2.Domain.Commands.GetAllCustomers;
+using PressStart2.Domain.Commands.GetSale;
 
 namespace PressStart2.Api.Controllers
 {
@@ -17,13 +18,22 @@ namespace PressStart2.Api.Controllers
             _mediator = mediator;
         }
 
+        [HttpGet("listar")]
+        public async Task<IActionResult> GetAll(Guid id)
+        {
+            var salesList = await _mediator.Send(new GetAllSalesRequest());
+            return Ok(salesList);
+        }
+
         [HttpPost("adicionar")]
         public async Task<IActionResult> Create(CreateSaleRequest request)
         {
             var response = await _mediator.Send(request);
             if (response.Success)
                 return Created("", response);
+
             return BadRequest(response);
         }
     }
 }
+ 
---
+++ backend/PressStart2/PressStart2.Domain/DTOs/SaleItemCreateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace PressStart2.Domain.DTOs
+{
+    public class SaleItemCreateDTO
+    {
+        public string ItemDescription { get; set; }
+        public decimal UnitaryValue { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalValue { get; set; }
+    }
+}
