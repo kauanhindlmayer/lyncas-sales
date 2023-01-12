b7b16077769c326d49f6fd981ab8e1d8691fe1aa
Kauan <kauanhindlmayer07@gmail.com>
2023-01-12 08:31:30 -0300

    feat: add handleExpiredToken

--- frontend/src/js/services/api.service.js
+++ frontend/src/js/services/api.service.js
@@ -3,54 +3,77 @@ import { user, handleExpiredToken } from "./user.service.js";
 
 export const api = {
   async get(resource) {
-    const response = await fetch(`${config.baseURL}/${resource}/listar`, {
-      method: "GET",
-      headers: { Authorization: `Bearer ${user.token}` },
-    });
-    return await response.json();
+    try {
+      const response = await fetch(`${config.baseURL}/${resource}/listar`, {
+        method: "GET",
+        headers: { Authorization: `Bearer ${user.token}` },
+      });
+      return await response.json();
+    } catch (error) {
+      handleExpiredToken();
+    }
   },
 
   async getById(resource, id) {
-    const response = await fetch(`${config.baseURL}/${resource}/obter/${id}`, {
-      method: "GET",
-      headers: { Authorization: `Bearer ${user.token}` },
-    });
-    return await response.json();
+    try {
+      const response = await fetch(
+        `${config.baseURL}/${resource}/obter/${id}`,
+        {
+          method: "GET",
+          headers: { Authorization: `Bearer ${user.token}` },
+        }
+      );
+      return await response.json();
+    } catch (error) {
+      handleExpiredToken();
+    }
   },
 
   async post(resource, body) {
-    const path = resource === "User" ? "autenticar" : "adicionar";
-    const response = await fetch(`${config.baseURL}/${resource}/${path}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${user.token}`,
-      },
-      body: JSON.stringify(body),
-    });
-    return await response.json();
+    try {
+      const path = resource === "User" ? "autenticar" : "adicionar";
+      const response = await fetch(`${config.baseURL}/${resource}/${path}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+        body: JSON.stringify(body),
+      });
+      return await response.json();
+    } catch (error) {
+      handleExpiredToken();
+    }
   },
 
   async put(resource, body) {
-    const response = await fetch(`${config.baseURL}/${resource}/atualizar`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${user.token}`,
-      },
-      body: JSON.stringify(body),
-    });
-    return await response.json();
+    try {
+      const response = await fetch(`${config.baseURL}/${resource}/atualizar`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+        body: JSON.stringify(body),
+      });
+      return await response.json();
+    } catch (error) {
+      handleExpiredToken();
+    }
   },
 
   async delete(resource, id) {
-    const response = await fetch(
-      `${config.baseURL}/${resource}/remover/${id}`,
-      {
-        method: "DELETE",
-        headers: { Authorization: `Bearer ${user.token}` },
-      }
-    );
-    return await response.json();
+    try {
+      const response = await fetch(
+        `${config.baseURL}/${resource}/remover/${id}`,
+        {
+          method: "DELETE",
+          headers: { Authorization: `Bearer ${user.token}` },
+        }
+      );
+      return await response.json();
+    } catch (error) {
+      handleExpiredToken();
+    }
   },
 };
