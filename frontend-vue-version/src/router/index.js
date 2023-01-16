7b49a3db48ab4ca2aa4a2eb792138de512a8a9a0
Kauan <kauanhindlmayer07@gmail.com>
2023-01-16 11:33:06 -0300

    feat: add 404 route

--- frontend-vue-version/src/router/index.js
+++ frontend-vue-version/src/router/index.js
@@ -1,86 +1,75 @@
 import { createRouter, createWebHistory } from "vue-router";
 import checkAuth from "../middlewares/checkAuth.js";
 
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: () => import("../views/Dashboard.vue"),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: () => import("../views/Dashboard.vue"),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/lista-de-vendas",
+    name: "sales-list",
+    component: () => import("../views/SaleList.vue"),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/lista-de-clientes",
+    name: "customers-list",
+    component: () => import("../views/CustomerList.vue"),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/adicionar-cliente",
+    name: "create-customer",
+    component: () => import("../views/CustomerCreate.vue"),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/adicionar-venda",
+    name: "create-sale",
+    component: () => import("../views/SaleCreate.vue"),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/atualizar-cliente",
+    name: "update-customer",
+    component: () => import("../views/CustomerUpdate.vue"),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/atualizar-venda",
+    name: "update-sale",
+    component: () => import("../views/SaleUpdate.vue"),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/conectar-se",
+    name: "login",
+    component: () => import("../views/LoginForm.vue"),
+  },
+  {
+    path: "/criar-conta",
+    name: "register",
+    component: () => import("../views/RegisterForm.vue"),
+  },
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("../views/404.vue"),
+  },
+];
+
 const router = createRouter({
-  mode: "history",
   history: createWebHistory(import.meta.env.BASE_URL),
   linkExactActiveClass: "menu__item--active",
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: () => import("../views/Dashboard.vue"),
-      meta: {
-        requiresAuth: true,
-      },
-    },
-    {
-      path: "/conectar-se",
-      name: "login",
-      component: () => import("../views/LoginForm.vue"),
-    },
-    {
-      path: "/criar-conta",
-      name: "register",
-      component: () => import("../views/RegisterForm.vue"),
-    },
-    {
-      path: "/dashboard",
-      name: "dashboard",
-      component: () => import("../views/Dashboard.vue"),
-      meta: {
-        requiresAuth: true,
-      },
-    },
-    {
-      path: "/lista-de-vendas",
-      name: "sales-list",
-      component: () => import("../views/SaleList.vue"),
-      meta: {
-        requiresAuth: true,
-      },
-    },
-    {
-      path: "/lista-de-clientes",
-      name: "customers-list",
-      component: () => import("../views/CustomerList.vue"),
-      meta: {
-        requiresAuth: true,
-      },
-    },
-    {
-      path: "/adicionar-cliente",
-      name: "create-customer",
-      component: () => import("../views/CustomerCreate.vue"),
-      meta: {
-        requiresAuth: true,
-      },
-    },
-    {
-      path: "/adicionar-venda",
-      name: "create-sale",
-      component: () => import("../views/SaleCreate.vue"),
-      meta: {
-        requiresAuth: true,
-      },
-    },
-    {
-      path: "/atualizar-cliente",
-      name: "update-customer",
-      component: () => import("../views/CustomerUpdate.vue"),
-      meta: {
-        requiresAuth: true,
-      },
-    },
-    {
-      path: "/atualizar-venda",
-      name: "update-sale",
-      component: () => import("../views/SaleUpdate.vue"),
-      meta: {
-        requiresAuth: true,
-      },
-    },
-  ],
+  routes,
 });
 
 router.beforeEach((to, from, next) => {
---
+++ frontend-vue-version/src/views/404.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="content__heading">
+    <h1>Página não encontrada :(</h1>
+    <p>A página que você tentou acessar está indisponível ou não existe.</p>
+  </div>
+</template>
+
+<style scoped>
+.content__heading {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.content__heading h2 {
+  font-size: 1.5em;
+  font-weight: 500;
+}
+</style>
